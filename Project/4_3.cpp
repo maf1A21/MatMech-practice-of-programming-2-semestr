c9e34f87f16caa025a62f2502dea903b35a237ba
Jurabek Aminov <61906886+maf1A21@users.noreply.github.com>
2021-06-08 01:07:27 +0300

    Create 4_3.cpp

---
+++ Project/4_3.cpp
@@ -0,0 +1,10 @@
+// Используем перегрузку operator[] для ArrayInt
+ 
+try
+{
+    int *value = new int(array[index1] + array[index2]);
+}
+catch (int value)
+{
+    // Какие исключения мы здесь ловим?
+}
