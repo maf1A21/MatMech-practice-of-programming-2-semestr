3c025eba040c777263d20cc3d01da800b8066fbc
Jurabek Aminov <61906886+maf1A21@users.noreply.github.com>
2021-06-07 21:07:03 +0300

    Create 1_1.cpp

---
+++ Project/1_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int Find_letter_pos(const char* str, char c)
+{
+  for (size_t i = 0; size < strlen(str); ++i)
+  {
+    if (str[i] == c)
+      return pos;
+  }
+  return -1;
+}
