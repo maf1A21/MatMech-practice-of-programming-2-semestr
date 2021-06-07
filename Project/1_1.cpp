d2079b3429f23c2c7e0cbda2d76527cce9975cf1
Jurabek Aminov <61906886+maf1A21@users.noreply.github.com>
2021-06-07 21:21:01 +0300

    Update 1_1.cpp

--- Project/1_1.cpp
+++ Project/1_1.cpp
@@ -8,5 +8,20 @@ int Find_letter_pos(const char* str, char c)
     if (str[i] == c)
       return pos;
   }
-  return -1;
+  return -1; // ничего не говорит
+}
+
+struct Date_of_birth
+{
+  int day;
+  int month;
+  int year;
+  Date_of_birth(int a, int b, int c)
+  {
+    if (( a < 1) || (a > 31) || ( b < 1) || (b > 12) || (c > 2021)) // понятно, что не все случаи, но для примера хватит
+    {return -1;} // не-а, это конструктор, никаких return!
+    day = a;
+    month = b;
+    year = c;
+  }
 }
